0b08811db1500dc43f0e06722f45e37f4adbd814
KangOxford <kang.li@maths.ox.ac.uk>
2022-07-08 10:44:00 +0100

    initial

---
+++ for_test.py
@@ -0,0 +1,10 @@
+# %%
+import subprocess
+subprocess.call(['sh', './scripts/run_gail.sh'])
+# %%
+import pandas as pd
+import numpy as np
+print(np.arange(10))
+# %%
+import subprocess
+process = subprocess.run('./scripts/run_gail.sh', shell=True, check=True)
